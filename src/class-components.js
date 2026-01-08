5f82b4066ab04522a498e57c47e4e47448231b53
pat123 <phollakrit@hotmail.com>
2026-01-08 10:25:14 +0700

    Problem with Event Handler

--- src/class-components.js
+++ src/class-components.js
@@ -1,22 +1,15 @@
 import React,{Component} from "react";
 
-export default class Calendar extends Component{
-    getDate(){
-        const dayNames=['Sunday','Monday','Tuesday','Wednesday'
-            ,'Thursday','Friday','Saturday']
-        const monthNames=['January','February','March','April'
-            ,'May','June','July','August','September','October'
-            ,'November','December']
-        const date=new Date()
-        const weekDay=dayNames[date.getDay()]
-        const day=date.getDate()
-        const month=monthNames[date.getMonth()]
-        const year=date.getFullYear()
-        return `${weekDay} ${day} ${month} ${year}`
+export default class Button extends Component{
+    
+    showAlert(msg){
+        alert(msg)
     }
-
+    onClickButtonOK=()=>{
+        this.showAlert('Hello')
+    }
     render(){
-        return <div>{this.getDate()}</div>
+        return <button onClick={this.onClickButtonOK}>OK</button>
     }
 }
 
