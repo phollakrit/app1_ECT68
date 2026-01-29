80d5104f6fc8ec0860258389c47d5410915a2c34
pat123 <phollakrit@hotmail.com>
2026-01-29 10:06:15 +0700

    Add context.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
-
-
-import MessageBox from "./state-func";
-
+import { userContext } from "./context";
+import Content from "./context-content";
 
 function App() {
-  return <MessageBox/>
+  return(
+    <userContext.Provider value={'Tom Jerry'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
 
 export default App;
